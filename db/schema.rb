4504bd8e6b1fb2e6de6bf72d36e1f7680557697e
Neeraj Kumar <neeraj.kumar@gmail.com>
2015-11-04 15:45:13 +0530

    adding correct mysql2 gem

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,11 @@
 ActiveRecord::Schema.define(version: 20151104072247) do
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.string   "author"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "title",       limit: 255
+    t.string   "author",      limit: 255
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
 end
