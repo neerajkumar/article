d0eb317569d9fc0999a6be24c05aed9e154a48b9
Neeraj Kumar <neeraj.kumar@gmail.com>
2015-11-04 15:49:37 +0530

    adding scaffolding of comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104072247) do
+ActiveRecord::Schema.define(version: 20151104101556) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author",      limit: 255
+    t.text     "description", limit: 65535
+    t.integer  "post_id",     limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",       limit: 255
